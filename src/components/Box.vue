2bb0772cbcafeed9c2e7d75140e145eed7f04a54
Cristine Acocella <rm88251@fiap.com.br>
2022-05-26 20:05:47 -0300

    style

---
+++ src/components/Box.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="div-container">
+    <div class="div-box">
+      <div class="div-grid">
+        <div class="title">
+          <p>Relação de Operadoras Ativas ANS</p>
+        </div>
+        <div class="search-box">
+          <input type="search" name="search" class="search" />
+        </div>
+        <div class="table"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoxBox",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.div-container {
+  margin-top: 0.5rem;
+  background-color: #f2f2f2;
+}
+
+.div-box {
+  display: inline-block;
+  background-color: white;
+  margin: 2rem 0;
+  width: 98%;
+}
+
+.div-grid {
+  padding: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  row-gap: 2rem;
+  align-content: space-between;
+}
+
+.search-box {
+  justify-self: flex-end;
+  width: 50%;
+}
+.search {
+  display: inline-block;
+  width: 100%;
+}
+
+.title {
+  justify-self: flex-start;
+  color: #4d4dff;
+}
+
+.title p {
+  font-size: 1.6rem;
+}
+
+.table {
+  grid-column: 1 / -1;
+  border: 0.1rem solid black;
+  height: 25rem;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="container-header">
+    <header class="header">
+      <div class="header-div">
+        <img
+          src="https://static.wixstatic.com/media/b91e09_95ec298e6ed645d197af640983985c6c~mv2.png/v1/fill/w_468,h_112,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logo_ic_v5_20210604_a4_logo_hQ.png"
+          alt="Logo Intuitive Care"
+        />
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderHeader",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.header-div {
+  display: flex;
+  justify-content: center;
+  padding: 2rem 0;
+  box-shadow: 0rem 0.5rem 0.5rem #cccccc;
+}
+
+.header-div img {
+  width: 15%;
+}
+</style>
